49062fd52c48c4d244fe8deb536f8d465805c329
samuelmetheny <samuelmetheny@gmail.com>
2025-01-16 15:30:00 -0600

    mail update

---
+++ assets/js/mail.js
@@ -0,0 +1,20 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "Wb19WXrOdOntMLMdX",
+    });
+})();
+
+window.onload = function() {
+    console.log('loaded');
+    document.getElementById('contactform').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_8gspsn6', 'template_zrbbqbo', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
